14800230c0e375f0ce578757c829598da0653149
Dinoo16 <tranphuctrong16103@gmail.com>
2025-03-18 14:09:58 +0700

    music-web

---
+++ src/main/java/com/example/Music_Web/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.Music_Web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+
+        return "pages/home";
+    }
+
+    @GetMapping("/signin")
+    public String sign() {
+        return "pages/signin";
+    }
+
+}
